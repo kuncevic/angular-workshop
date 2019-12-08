5117132d283c8dd6ef21ad2cf9a444d97a08b174
Ckpinka <bobrovsk.ir@gmail.com>
2019-12-08 12:53:27 +0300

    2-b-angular-cli. Updated routes.

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { VideoComponent } from './video/video.component';
+import { VideosComponent } from './videos/videos.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: VideosComponent },
+  { path: 'video/:videoId', component: VideoComponent },
+  { path: '**', redirectTo: '' }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
