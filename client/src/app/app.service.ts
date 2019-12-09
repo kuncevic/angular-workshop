a5c0fcddcb27be838b1d270bfd0cc7d37216f473
valentinkononov <valentin.kononov@mapbox.com>
2019-12-09 16:32:41 +0300

    feat(service): correct video service implementation, add more variants

--- client/src/app/app.service.ts
+++ client/src/app/app.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import {catchError, map, retry} from 'rxjs/operators';
 import { environment } from '../environments/environment';
 import { VideoModel } from './video.model';
 
@@ -10,23 +10,43 @@ import { VideoModel } from './video.model';
 })
 export class AppService {
 
-  constructor(
-    private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) { }
 
   getVideos(): Observable<VideoModel[]> {
-      return this.httpClient.get(`${environment.apiUrl}/video`)
-        .pipe(map((data: any[]) => {
-          return data.map(x => {
-            const video: VideoModel = {
-              id: x.id,
-              title: x.title,
-              likes: x.likes,
-              dislikes: x.dislikes,
-              views: x.views,
-            };
-            return video;
-          });
-        }));
+      return this.httpClient.get<VideoModel[]>(`${environment.apiUrl}/video`);
   }
 
+  getVideoIds(): Observable<string[]> {
+    return this.httpClient.get<VideoModel[]>(`${environment.apiUrl}/video`)
+      .pipe(map((data: any[]) => {
+        return data.map(x => x.id);
+      }));
+  }
+
+  getVideosErrorChecked(): Observable<VideoModel[]> {
+    return this.httpClient.get<VideoModel[]>(`${environment.apiUrl}/video`)
+      .pipe(
+        retry(3),
+        catchError((error: any, caught: Observable<VideoModel[]>) => {
+          console.log(error);
+          return caught;
+        })
+      );
+  }
+
+  getVideoById(id: string): Observable<any> {
+    return this.httpClient.get(`${environment.apiUrl}/video/${id}`);
+  }
+
+  getWithParams(param1: number, param2: string): Observable<any> {
+    const params = new HttpParams();
+    params.set('param1', param1.toString());
+    params.set('param2', param2);
+
+    return this.httpClient.get<VideoModel[]>(`${environment.apiUrl}/video`, { params });
+  }
+
+  post(data: any): Observable<any> {
+    return this.httpClient.post(`${environment.apiUrl}/video`, data);
+  }
 }
