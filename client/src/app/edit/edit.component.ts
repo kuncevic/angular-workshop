9bc28f1ac2b218f3fe7772de294c35d0ae826ed3
kuncevic <kncvch@gmail.com>
2020-02-18 13:28:43 +1100

    add confirm dialog; minor refactor

--- client/src/app/edit/edit.component.ts
+++ client/src/app/edit/edit.component.ts
@@ -1,43 +1,43 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { FormBuilder } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { FormBuilder } from "@angular/forms";
 
-import { VideoService } from '../video.service';
-import { VideoModel } from '../video.model';
-
+import { VideoService } from "../video.service";
+import { VideoModel } from "../video.model";
 
 @Component({
-  selector: 'app-edit',
-  templateUrl: './edit.component.html',
-  styleUrls: ['./edit.component.scss'],
+  selector: "app-edit",
+  templateUrl: "./edit.component.html",
+  styleUrls: ["./edit.component.scss"]
 })
 export class EditComponent implements OnInit {
-
   form = this.fb.group({
-    title: '',
-    likes: '',
-    dislikes: '',
+    title: "",
+    likes: "",
+    dislikes: ""
   });
 
   private videoId = this.route.snapshot.params.videoId;
   private video: VideoModel;
 
-  constructor(private route: ActivatedRoute,
-              private fb: FormBuilder,
-              private videoService: VideoService) {
-  }
+  constructor(
+    private route: ActivatedRoute,
+    private fb: FormBuilder,
+    private videoService: VideoService
+  ) {}
 
   ngOnInit() {
-    this.videoService.getVideo(this.videoId)
-      .subscribe(video => {
-        this.video = video;
-        this.form.patchValue(video);
-      });
+    this.videoService.getVideo(this.videoId).subscribe(video => {
+      this.video = video;
+      this.form.patchValue(video);
+    });
   }
 
   submit() {
-    const update: Partial<VideoModel> = this.form.value;
-    const video: VideoModel = { ...this.video, ...update };
-    this.videoService.updateVideo(video).subscribe();
+    if(confirm('Are you sure?')) {
+      const update: Partial<VideoModel> = this.form.value;
+      const video: VideoModel = { ...this.video, ...update };
+      this.videoService.updateVideo(video).subscribe();
+    }
   }
 }
