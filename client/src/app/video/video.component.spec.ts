43021779d78c83b520d3fdf73ff2b09a44ff04cf
kuncevic <kncvch@gmail.com>
2021-03-16 19:23:49 +1100

    Update packages

--- client/src/app/video/video.component.spec.ts
+++ client/src/app/video/video.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { VideoComponent } from './video.component';
 
@@ -6,7 +6,7 @@ describe('VideoComponent', () => {
   let component: VideoComponent;
   let fixture: ComponentFixture<VideoComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ VideoComponent ]
     })
